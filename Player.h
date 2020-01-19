c5551a847a1d7ef50afabae7784924945f6eca24
Anton Vogel <vogel@itq.de>
2020-01-19 16:18:32 +0100

    Player added

--- Player.h
+++ Player.h
@@ -2,11 +2,16 @@
 #define PLAYER_H
 
 #include <QObject>
+#include <QString>
 
-class Player
-{
+class Player {
 public:
-    Player();
+  Player();
+  Player(int health, QString name);
+
+private:
+  int m_health;
+  QString m_playerName;
 };
 
 #endif // PLAYER_H
